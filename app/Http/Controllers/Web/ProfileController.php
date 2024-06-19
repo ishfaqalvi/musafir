5f4853b1c6538618ab7e9a3a5eea901126103ee5
Ishfaq Alvi <ishfaq.alvi.33@gmail.com>
2024-06-19 14:47:10 +0500

    Update profile info route

--- app/Http/Controllers/Web/ProfileController.php
+++ app/Http/Controllers/Web/ProfileController.php
@@ -13,7 +13,7 @@ class ProfileController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function accountInfo()
+    public function accountInfoForm()
     {
         return view('web.profile.account-info');
     }
